e9faed84da6264ba29238dec0e7ac4822925c0dc
JacopoLombardi <lombajacopo@gmail.com>
2024-05-16 15:59:41 +0200

    [add] created Migration & Model

---
+++ database/migrations/2024_05_16_135238_create_comics_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->text('description');
+            $table->string('image');
+            $table->string('price', 20);
+            $table->string('series', 50);
+            $table->string('sale_date', 30);
+            $table->string('type', 40);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
