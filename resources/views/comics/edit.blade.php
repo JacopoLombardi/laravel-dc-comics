1d74fa2ccdc4398a804abf0c5529cf11233c170b
JacopoLombardi <lombajacopo@gmail.com>
2024-05-20 16:16:39 +0200

    [add] completed view Edit with management of error

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -11,33 +11,113 @@
         <form class="col-6 mt-5" action="{{ route('comics.update', $comic) }}" method="POST">
             @csrf
             @method('PUT')
+
             <div>
                 <label class="fw-semibold">Titolo</label>
-                <input class="form-control" type="text" placeholder="inserire titolo" name="title" value="{{ $comic->title }}">
+                <input
+                  class="form-control @error('title') is-invalid @enderror"
+                  type="text"
+                  placeholder="inserire titolo"
+                  name="title"
+                  value="{{ old('title', $comic->title) }}"
+                >
+
+                @error('title')
+                    <h6 class="text-danger ms-3 mt-1">{{ $message }}</h6>
+                @enderror
             </div>
+
+
+
+
             <div class="my-4">
                 <label class="fw-semibold">Prezzo</label>
-                <input class="form-control" type="text" placeholder="inserire prezzo" name="price" value="{{ $comic->price }}">
+                <input
+                  class="form-control @error('price') is-invalid @enderror"
+                  type="text"
+                  placeholder="inserire prezzo"
+                  name="price"
+                  value="{{ old('price', $comic->price) }}"
+                >
+
+                @error('price')
+                    <h6 class="text-danger ms-3 mt-1">{{ $message }}</h6>
+                @enderror
             </div>
+
+
+
             <div>
                 <label class="fw-semibold">Serie</label>
-                <input class="form-control" type="text" placeholder="inserire serie" name="series" value="{{ $comic->series }}">
+                <input
+                  class="form-control @error('series') is-invalid @enderror"
+                  type="text"
+                  placeholder="inserire serie"
+                  name="series"
+                  value="{{ old('series', $comic->series) }}"
+                >
+
+                @error('series')
+                    <h6 class="text-danger ms-3 mt-1">{{ $message }}</h6>
+                @enderror
             </div>
+
+
+
             <div class="my-4">
                 <label class="fw-semibold">URL Immagine</label>
-                <input class="form-control" type="text" placeholder="inserire serie" name="image" value="{{ $comic->image }}">
+                <input
+                  class="form-control @error('image') is-invalid @enderror"
+                  type="text"
+                  placeholder="inserire serie"
+                  name="image"
+                  value="{{ old('image', $comic->image) }}"
+                >
+
+                @error('image')
+                    <h6 class="text-danger ms-3 mt-1">{{ $message }}</h6>
+                @enderror
             </div>
+
+
+
             <div>
                 <label class="fw-semibold">Data di Uscita</label>
-                <input class="form-control" type="text" placeholder="inserire serie" name="sale_date" value="{{ $comic->sale_date }}">
+                <input
+                  class="form-control @error('sale_date') is-invalid @enderror"
+                  type="text"
+                  placeholder="inserire serie"
+                  name="sale_date"
+                  value="{{ old('sale_date', $comic->sale_date) }}"
+                >
+
+                @error('sale_date')
+                    <h6 class="text-danger ms-3 mt-1">{{ $message }}</h6>
+                @enderror
             </div>
+
+
+
             <div class="my-4">
                 <label class="fw-semibold">Tipo di Fumetto</label>
-                <input class="form-control" type="text" placeholder="inserire serie" name="type" value="{{ $comic->type }}">
+                <input
+                  class="form-control @error('type') is-invalid @enderror"
+                  type="text"
+                  placeholder="inserire serie"
+                  name="type"
+                  value="{{ old('type', $comic->type) }}"
+                >
+
+                @error('type')
+                    <h6 class="text-danger ms-3 mt-1">{{ $message }}</h6>
+                @enderror
             </div>
+
+
+
             <div>
                 <label class="fw-semibold">Descrizione</label>
-                <textarea class="form-control" name="description" cols="30" rows="5">{{ $comic->description }}"</textarea>
+                <textarea class="form-control" name="description" cols="30" rows="5">{{ $comic->description }}</textarea>
             </div>
 
             <button class="btn btn-warning fw-semibold mt-5">Conferma le Modifiche</button>
